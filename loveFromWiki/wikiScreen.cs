0f3a4cdef73c75f316812fe2c750aae883933596
Charles <charles@klei.vip>
2025-03-22 01:05:21 +0800

    能跳转了

--- loveFromWiki/wikiScreen.cs
+++ loveFromWiki/wikiScreen.cs
@@ -1,32 +1,51 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
 
 namespace loveFromWiki
 {
     public class WikiScreen : DetailScreenTab
+    {
+        public override bool IsValidForTarget(GameObject target)
         {
-            public override bool IsValidForTarget(GameObject target)
-            {
-                return true;
-            }
-            protected override void OnPrefabInit()
-            {
-                base.OnPrefabInit();
-                this.A = base.CreateCollapsableSection("");
-                
-            }
-            protected override void OnSelectTarget(GameObject target) { }
-            public override void OnDeselectTarget(GameObject target) { }
-            protected override void Refresh(bool force = false)
-            {
-                WikiScreen.RefreshA(this.A, this.selectedTarget);
-            }
-            private static void RefreshA(CollapsibleDetailContentPanel targetPanel, GameObject targetEntity)
-            {
-                targetPanel.SetActive(true);
-                targetPanel.SetLabel("这个是id", "这是名字", "不知道填什么");
-                targetPanel.Commit();
-            }
+            return true;
+        }
+        protected override void OnPrefabInit()
+        {
+            base.OnPrefabInit();
+            A = base.CreateCollapsableSection("这是栏目A");
+            B = base.CreateCollapsableSection("这是栏目B");
 
-            public CollapsibleDetailContentPanel A;
+        }
+        protected override void OnSelectTarget(GameObject target) 
+        {
+            base.OnSelectTarget(target);
+        }
+        public override void OnDeselectTarget(GameObject target) { }
+        protected override void Refresh(bool force = false)
+        {
+            WikiScreen.RefreshA(this.A, this.selectedTarget);
+            WikiScreen.RefreshB(this.B, this.selectedTarget);
+        }
+        private static void RefreshA(CollapsibleDetailContentPanel targetPanel, GameObject targetEntity)
+        {
+            targetPanel.SetActive(true);
+            targetPanel.SetLabel("这个是id1", "这是名字1", "不知道填什么");
+            targetPanel.Commit();
+        }
+
+        private static void RefreshB(CollapsibleDetailContentPanel targetPanel, GameObject targetEntity)
+        {
+            targetPanel.SetActive(true);
+            targetPanel.SetLabelWithButton("BWIKI", "跳转到BWIKI镜像站点", "跳转到镜像站点", delegate
+            {
+                string url = "https://wiki.biligame.com/oni/%E9%A6%96%E9%A1%B5";
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            });
+            targetPanel.Commit();
+        }
+
+        public CollapsibleDetailContentPanel A;
+        public CollapsibleDetailContentPanel B;
     }
 }
