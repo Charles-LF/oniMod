219acf55e145c5778e9a0f7d785c1a00539dbf2c
Charles Lf <charles@klei.vip>
2025-04-14 01:37:08 +0800

    成功添加了按钮和布局

--- loveFromWiki/Patches.cs
+++ loveFromWiki/Patches.cs
@@ -15,15 +15,15 @@ namespace loveFromWiki
         [HarmonyPatch(typeof(DetailTabHeader), "Init")]
         public class DetailTabHeader_Init_Patch
         {
-            [HarmonyPostfix]
             public static void Postfix(DetailTabHeader __instance)
             {
                 var tabName = "WIKITAB";
                 var title = "WIKI";
                 var desc = "<b>查看WIKI</b>\n爱来自缺氧中文维基";
-                // 初始化wikiScreen
+                
                 var wikiGameObject = new GameObject("wikiGameObject");
                 var wikiScreen = wikiGameObject.AddComponent<WikiScreen>();
+
                 // 使用反射调用私有方法 MakeTab
                 var makeTabMethod =
                     typeof(DetailTabHeader).GetMethod("MakeTab", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -35,15 +35,6 @@ namespace loveFromWiki
                         Debug.LogError("未能获取到指定的图！");
                         return;
                     }
-
-                    // 检查 wikiScreen 是否为 null
-                    if (wikiScreen == null)
-                    {
-                        Debug.LogError("wikiScreen 未正确初始化！");
-                        return;
-                    }
-
-                    //Debug.Log($"即将调用 MakeTab 方法，wikiScreen 是否为 null: {wikiScreen == null}");
                     // 传递WikiScreen组件所在的GameObject
                     makeTabMethod.Invoke(__instance,
                         new object[] { tabName, title, sprite, desc, wikiScreen.gameObject });
--- loveFromWiki/wikiScreen.cs
+++ loveFromWiki/wikiScreen.cs
@@ -1,12 +1,15 @@
-﻿using System.Diagnostics;
+﻿using PeterHan.PLib.UI;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace loveFromWiki
 {
     public class WikiScreen : DetailScreenTab
     {
-        public CollapsibleDetailContentPanel wikiPanel;
+        private CollapsibleDetailContentPanel wikiPanel;
+        private static string itemName = "";
 
         public override bool IsValidForTarget(GameObject target)
         {
@@ -16,41 +19,68 @@ namespace loveFromWiki
         protected override void OnPrefabInit()
         {
             base.OnPrefabInit();
-            wikiPanel = CreateCollapsableSection("WIKI面板");
-            wikiPanel.GetComponent<Transform>().localPosition = new Vector3(0, -25, 0);
-        }
+            // 所有UI对象必须包含RectTransform
+            gameObject.AddComponent<RectTransform>();
+            // 需要自动布局时添加
+            gameObject.AddOrGet<ContentSizeFitter>();
+            var layout = gameObject.AddOrGet<VerticalLayoutGroup>();
+            layout.spacing = 10f;          // 子对象间距10像素
+            layout.padding = new RectOffset(5, 5, 5, 5); // 上下左右边距5像素
+            layout.childAlignment = TextAnchor.UpperCenter; // 子对象顶部居中
 
-        protected override void OnSelectTarget(GameObject target)
-        {
-            base.OnSelectTarget(target);
-            wikiPanel.gameObject.SetActive(true);
-            Refresh(true);
-        }
+            gameObject.AddOrGet<LayoutElement>();
+            gameObject.AddOrGet<CanvasRenderer>();
+            gameObject.AddOrGet<Image>();
 
-        public override void OnDeselectTarget(GameObject target)
-        {
-            base.OnDeselectTarget(target);
+            wikiPanel = CreateCollapsableSection("Charles的碎碎念");
+
         }
+        public override void OnDeselectTarget(GameObject target) { }
+        protected override void OnSelectTarget(GameObject target) { }
+
 
         protected override void Refresh(bool force = false)
         {
+            var baseurl = "https://wiki.biligame.com/oni/";
+            var ggurl = "https://oxygennotincluded.wiki.gg/zh/";
+
+            if (wikiPanel != null && !ButtonAdded)
+            {
+                GameObject ggButton = CreateCustomButton("ggButton", "GG原站点", "点击跳转到原站点", OnClickggButton);
+                ggButton.transform.SetParent(this.transform, false);
+                ggButton.transform.SetSiblingIndex(1);
+
+                GameObject bwikiButton = CreateCustomButton("bwikiButton", "bwiki镜像站", "点击跳转到镜像站", OnClickBwikiButton);
+                bwikiButton.transform.SetParent(this.transform, false);
+                bwikiButton.transform.SetSiblingIndex(2);
+                ButtonAdded = true;
+                Debug.Log("按钮创建成功（Refresh）");
+            }
+
+            void OnClickggButton()
+            {
+                KMonoBehaviour.PlaySound(GlobalAssets.GetSound("HUD_Click", false));
+                Process.Start(new ProcessStartInfo(ggurl+ itemName) { UseShellExecute = true });
+            };
+
+            void OnClickBwikiButton()
+            {
+                KMonoBehaviour.PlaySound(GlobalAssets.GetSound("HUD_Click", false));
+                Process.Start(new ProcessStartInfo(baseurl+ itemName) { UseShellExecute = true });
+            };
+
             RefreshWikiPanel(wikiPanel, selectedTarget);
         }
 
         private static void RefreshWikiPanel(CollapsibleDetailContentPanel targetPanel, GameObject targetEntity)
         {
-            var itemName = "";
-            targetPanel.SetActive(true);
-            var baseurl = "https://wiki.biligame.com/oni/";
-            var ggurl = "https://oxygennotincluded.wiki.gg/zh/";
             if (targetEntity != null)
             {
-                // targetEntity.GetComponent<KSelectable>()?.GetName() ?? " "  拿游戏内的名字
+                var name = targetEntity.GetComponent<KSelectable>()?.GetName() ?? " ";  //  拿游戏内的名字
                 // 这个鬼东西会返回 <link="DININGTABLE">餐桌</link> 这样的鬼东西，用正则提取名字
 
                 var pattern = @"<link=""[^""]+"">(.*?)</link>";
-                var regex = new Regex(pattern);
-                var match = Regex.Match(targetEntity.GetComponent<KSelectable>()?.GetName() ?? " ", pattern);
+                var match = Regex.Match(name, pattern);
                 if (match.Success)
                 {
                     // 提取 <link> 标签内的文本
@@ -58,14 +88,22 @@ namespace loveFromWiki
                     itemName = text;
                 }
             }
-
-            //targetPanel.SetLabel("url1", baseurl, baseurl);
-            //targetPanel.SetLabel("WIKILabel1", "点击下面按钮空就能跳转啦！", "皮肤名称没法识别");
-            targetPanel.SetLabelWithButton("BWIKI", "跳转到BWIKI镜像站点", "跳转到镜像站点",
-                delegate { Process.Start(new ProcessStartInfo(baseurl + itemName) { UseShellExecute = true }); });
-            targetPanel.SetLabelWithButton("GGWIKI", "跳转到WIKIGG源站点", "跳转到WIKIGG源站点",
-                delegate { Process.Start(new ProcessStartInfo(ggurl + itemName) { UseShellExecute = true }); });
+            targetPanel.SetActive(true);
+            targetPanel.SetLabel("WIKILabel1", "代码基本是抄游佬的\n\nc#基本是不会的\n\n皮肤名称是不会处理的\n\n最后\n\n点击上面按钮空就能跳转啦！", "Alright, go ahead and enjoy your game now!");
             targetPanel.Commit();
         }
+        private GameObject CreateCustomButton(string name, string text, string tooltip, System.Action onClick)
+        {
+            var btn = new PButton(name)
+            {
+                Text = text,
+                ToolTip = tooltip,
+                //Sprite = Assets.GetSprite("icon_display_screen_properties"),
+                OnClick = (go) => onClick?.Invoke()
+            }
+            .SetKleiBlueStyle();
+            return btn.Build();
+        }
+        private bool ButtonAdded = false;
     }
 }
